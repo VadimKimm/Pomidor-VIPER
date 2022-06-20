eab26c8a442c75017cd694419fee4aa076a0a9f2
Vadim <vadim_kimm@icloud.com>
2022-06-20 13:05:36 +0300

    Initial Commit

---
+++ Pomidor-VIPER/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pomidor-VIPER
+//
+//  Created by Vadim Kim on 20.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
