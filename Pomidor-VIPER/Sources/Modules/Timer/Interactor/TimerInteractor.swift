079be7d4feceb2c64a434b7bbd45fa348c814742
Vadim <vadim_kimm@icloud.com>
2022-06-21 23:37:20 +0300

    Created class TimerInteractor

--- Pomidor-VIPER/Sources/Modules/Timer/Interactor/TimerInteractor.swift
+++ Pomidor-VIPER/Sources/Modules/Timer/Interactor/TimerInteractor.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+
+protocol TimerInteractorInput: AnyObject {
+    var isWorkTime: Bool { get set }
+
+    init(presenter: TimerInteractorOutput)
+
+    func provideTimer()
+}
+
+protocol TimerInteractorOutput: AnyObject {
+    func receiveTimer(with timerData: TimerData)
+    func receiveTimerMode(isWorkTime: Bool)
+}
+
+class TimerInteractor: TimerInteractorInput {
+
+    unowned let presenter: TimerInteractorOutput
+    private var timer: TimerEntity
+
+    var isWorkTime: Bool {
+        get {
+            timer.isWorkTime
+        } set {
+            timer.isWorkTime.toggle()
+        }
+    }
+
+    required init(presenter: TimerInteractorOutput) {
+        self.presenter = presenter
+        self.timer = TimerEntity()
+    }
+
+    func provideTimer() {
+        let timerData = TimerData(timerLabel: timer.workTime,
+                                  isWorkTime: timer.isWorkTime)
+        presenter.receiveTimer(with: timerData)
+    }
+
+}
