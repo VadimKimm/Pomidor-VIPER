f79f08f3c09b5442e8a655cd1a8c482f00ad83e3
Vadim <vadim_kimm@icloud.com>
2022-06-21 23:37:54 +0300

    Created class TimerRouter

--- Pomidor-VIPER/Sources/Modules/Timer/Router/TimerRouter.swift
+++ Pomidor-VIPER/Sources/Modules/Timer/Router/TimerRouter.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+protocol TimerRouterInput {
+    init(viewController: TimerViewController)
+    func openSettingsViewConroller()
+}
+
+class TimerRouter: TimerRouterInput {
+    unowned let viewController: TimerViewController
+
+    required init(viewController: TimerViewController) {
+        self.viewController = viewController
+    }
+    
+
+    func openSettingsViewConroller() {
+        
+    }
+}
