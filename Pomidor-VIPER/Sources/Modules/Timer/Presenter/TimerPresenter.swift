98bd61d1ace97f938a2014f02598a7e41c44778a
Vadim <vadim_kimm@icloud.com>
2022-06-21 23:37:01 +0300

    Created class TimerPresenter

--- Pomidor-VIPER/Sources/Modules/Timer/Presenter/TimerPresenter.swift
+++ Pomidor-VIPER/Sources/Modules/Timer/Presenter/TimerPresenter.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+
+struct TimerData {
+    let timerLabel: Int
+    let isWorkTime: Bool
+}
+
+class TimerPresenter: TimerViewOutput {
+
+    unowned let view: TimerViewInput
+    var interactor: TimerInteractorInput!
+    var router: TimerRouter!
+
+    required init(view: TimerViewInput) {
+        self.view = view
+    }
+
+    func showDetails() {
+        interactor.provideTimer()
+    }
+
+    func startPauseButtonTapped() {
+
+    }
+}
+
+//MARK: - TimerInteractorOutput -
+extension TimerPresenter: TimerInteractorOutput {
+
+    func receiveTimer(with timerData: TimerData) {
+        let time = timerData.timerLabel
+        let timerLabelTextForWork = String("\(time < 10 ? "0\(time):00" : "\(time):00")")
+
+        view.displayTimerLabel(with: timerLabelTextForWork)
+        view.dislpayStartPauseButton(forModeIswork: timerData.isWorkTime)
+
+
+    }
+
+    func receiveTimerMode(isWorkTime: Bool) {
+
+    }
+}
