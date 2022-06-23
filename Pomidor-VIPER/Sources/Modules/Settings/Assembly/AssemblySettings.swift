bba15b2f2d4d0cab5d4a8ff46be7d5a26a8548a6
Vadim <vadim_kimm@icloud.com>
2022-06-23 11:26:42 +0300

    Created class AssemblySettings

---
+++ Pomidor-VIPER/Sources/Modules/Settings/Assembly/AssemblySettings.swift
@@ -0,0 +1,25 @@
+//
+//  AssemblySettings.swift
+//  Pomidor-VIPER
+//
+//  Created by t h a . m a m e rozz on 22.06.22.
+//
+
+import UIKit
+
+protocol AssemblySettingsInputs {
+    func AssemblyModule(with view: SettingsViewController)
+}
+
+class AssemblySettings: AssemblySettingsInputs {
+
+    func AssemblyModule(with view: SettingsViewController) {
+        let presenter = SettingsPresenter(view: view)
+        let interactor = SettingsInteractor(presenter: presenter)
+        let router = SettingsRouter(viewController: view)
+
+        view.presenter = presenter
+        presenter.interactor = interactor
+        presenter.router = router
+    }
+}
